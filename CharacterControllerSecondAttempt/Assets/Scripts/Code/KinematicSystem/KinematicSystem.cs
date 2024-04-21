e92c943bda0281bb326eb9956fc096b6dc98c122
Ricardo Cortes-Monroy <rj.cortesmonroy@gmail.com>
2024-04-21 14:02:13 +0100

    Meta

--- CharacterControllerSecondAttempt/Assets/Scripts/Code/KinematicSystem/KinematicSystem.cs
+++ CharacterControllerSecondAttempt/Assets/Scripts/Code/KinematicSystem/KinematicSystem.cs
@@ -18,7 +18,7 @@ public class KinematicSystem : MonoBehaviour
     private static Dictionary<int, KinematicBody> _bodies = new();
     private static Dictionary<int, KinematicMover> _movers = new();
 
-    private bool _interpolate = false;
+    private bool _interpolate = true;
 
     private int _fixedFrame;
     private float _simulationStartTime;
